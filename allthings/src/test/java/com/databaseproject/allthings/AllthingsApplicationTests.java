9ec78e73a9ba8e46f91bd39831027d3f79cc5168
Emmanuel Amanga <emmanuelamanga2013@gmail.com>
2023-04-25 18:46:31 +0300

    Initial Commit added shop

---
+++ allthings/src/test/java/com/databaseproject/allthings/AllthingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databaseproject.allthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllthingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
